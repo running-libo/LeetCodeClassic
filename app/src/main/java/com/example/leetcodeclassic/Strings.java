67db32007bf12d27304430af53c0831828cd7af9
libo <libo31@xiaomi.com>
2023-11-28 14:12:14 +0800

    无重复字符的最长子串解法修改

--- app/src/main/java/com/example/leetcodeclassic/Strings.java
+++ app/src/main/java/com/example/leetcodeclassic/Strings.java
@@ -27,27 +27,31 @@ public class Strings {
 
 //    ###### [无重复字符的最长子串](https://leetcode.cn/problems/longest-substring-without-repeating-characters/)
     class Solution {
-        public int lengthOfLongestSubstring(String s) {
-            // 哈希集合，记录每个字符是否出现过
-            Set<Character> occ = new HashSet<Character>();
-            int n = s.length();
-            // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-            int rk = -1, ans = 0;
-            for (int i = 0; i < n; ++i) {
-                if (i != 0) {
-                    // 左指针向右移动一格，移除一个字符
-                    occ.remove(s.charAt(i - 1));
-                }
-                while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
-                    // 不断地移动右指针
-                    occ.add(s.charAt(rk + 1));
-                    ++rk;
-                }
-                // 第 i 到 rk 个字符是一个极长的无重复字符子串
-                ans = Math.max(ans, rk - i + 1);
+    /**
+     * 思路：滑动窗口解法
+     * 以 (a)bcabcbb 开始的最长字符串为 (abc)abcbb
+     * 以 a(b)cabcbb 开始的最长字符串为 a(bca)bcbb
+     * ...
+     * 以 abcabc(b)b 开始的最长字符串为 abcabc(b)b
+     * 以 abcabcb(b) 开始的最长字符串为 abcabcb(b)
+     * 枚举每个位置开始下标为i，从这里开始找一个右指针rk，在一个范围内找无重复字符，用Hashset记录是否重复，直到遇到重复字符，则结束本次窗口。
+     * 每次循环一个i更新窗口的最大长度，并且清空set
+     */
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet();
+        int n = s.length();
+        int maxLeng = 0;
+        for (int i=0;i<n;i++) {
+            int right = i;
+            while(right < n && set.add(s.charAt(right))) {
+                //新字符添加成功，右移右指针
+                right++;
             }
-            return ans;
+            maxLeng = Math.max(maxLeng, set.size()); //set存放为i起始的、无重复字符的子字符集
+            set.clear();
         }
+        return maxLeng;
+    }
     }
 
     //    ###### [最长回文子串](https://leetcode.cn/problems/longest-palindromic-substring/)
